81eda51b2b66b0717fe3dca16ccea69115941617
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-01-31 19:40:00 -0300

    feat: create index page

--- social-media-belt/pages/index.tsx
+++ social-media-belt/pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+
+import SEOComponent from 'components/SEO'
 
 const Home: NextPage = () => {
-  return <h1>Social Media Belt</h1>
+  return (
+    <>
+      <SEOComponent />
+      <ul>
+        <li><Link href="/app">App</Link></li>
+        <li><Link href="/tenant">Tenant</Link></li>
+      </ul>
+    </>
+  )
 }
 
 export default Home
