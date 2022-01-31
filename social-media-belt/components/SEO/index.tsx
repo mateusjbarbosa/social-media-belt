c004295cdd9b15d6554753206046a49629ebf3ed
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-01-31 19:39:27 -0300

    feat: create seo component

---
+++ social-media-belt/components/SEO/index.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+const SEOComponent = () => (
+  <Head>
+    <title>Social Media Belt</title>
+    <meta name="description" content="Social Media Belt" />
+  </Head>
+)
+
+export default SEOComponent
