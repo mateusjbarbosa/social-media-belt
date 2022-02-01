ade2a174797bd20cac1492d2a31fcd9f231fa5cf
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-01 11:42:59 -0300

    feat: prepare _app page to dynamic layout on pathname based

--- social-media-belt/pages/_app.tsx
+++ social-media-belt/pages/_app.tsx
@@ -1,9 +1,35 @@
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+
+import LayoutApp from 'components/Layouts/LayoutApp'
+import LayoutPublic from 'components/Layouts/LayoutPublic'
+import LayoutTenant from 'components/Layouts/LayoutTenant'
 
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  const { pathname } = router
+
+  let Layout = undefined
+
+  function verifyPathname(match: string): boolean {
+    return pathname.indexOf(match) === 0
+  }
+
+  if (verifyPathname('/app')) {
+    Layout = LayoutApp
+  } else if (verifyPathname('/[slug]')) {
+    Layout = LayoutTenant
+  } else {
+    Layout = LayoutPublic
+  }
+
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
