67d491ee04d3ff172a5e27d58c6d990e8e162dee
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-01 11:42:17 -0300

    feat: change SEO component to dynamic props

--- social-media-belt/components/SEO/index.tsx
+++ social-media-belt/components/SEO/index.tsx
@@ -1,9 +1,14 @@
 import Head from "next/head";
 
-const SEOComponent = () => (
+type Props = {
+  title: string
+  description?: string
+}
+
+const SEOComponent = ({ title, description }: Props) => (
   <Head>
-    <title>Social Media Belt</title>
-    <meta name="description" content="Social Media Belt" />
+    <title>{title}</title>
+    {description && <meta name="description" content={description} />}
   </Head>
 )
 
