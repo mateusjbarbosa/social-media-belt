5c72de51deb9dd8b747af8d96899695789088b3e
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-14 10:56:58 -0300

    feat: change fetch and swr to custom components and use alert component with no link data

--- pages/app/[tenantId]/links.tsx
+++ pages/app/[tenantId]/links.tsx
@@ -3,16 +3,16 @@ import { useRouter } from "next/router"
 import { SubmitHandler, useForm } from "react-hook-form"
 import { yupResolver } from '@hookform/resolvers/yup'
 
-import useSWR from "swr"
-
 import * as yup from "yup"
 
 import { Link } from '@prisma/client'
 
+import { useGet } from "hooks/api"
+import { fetchPost, fetchDelete } from "lib/fetch"
+
+import AlertComponent from "components/Alert"
 import Subtitle from "components/Headings/Subtitle"
 import Title from "components/Headings/Title"
-
-const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 type NewLinkForm = {
   name: string
@@ -33,20 +33,22 @@ const newLinkSchema = yup.object({
 const Links = () => {
   const router = useRouter()
 
-  const { data } = useSWR(`/api/${router?.query?.tenantId}/links`, fetcher)
+  const { data, mutate } = useGet<Link[]>(`/api/${router?.query?.tenantId}/links`)
 
   const { handleSubmit, register, formState: { errors } } = useForm<NewLinkForm>({
     resolver: yupResolver(newLinkSchema)
   })
 
-  const submit: SubmitHandler<NewLinkForm> = async (inputs: NewLinkForm) => {
-    const data = await fetch(`api/${router?.query?.tenantId}/links`, {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(inputs)
-    })
+  const submitLink: SubmitHandler<NewLinkForm> = async (inputs: NewLinkForm) => {
+    await fetchPost({ url: `api/${router?.query?.tenantId}/links`, data: inputs })
+
+    await mutate()
+  }
+
+  const deleteLink = async (id: string) => {
+    await fetchDelete({ url: `api/${router?.query?.tenantId}/links/${id}` })
+
+    await mutate()
   }
 
   return (
@@ -74,8 +76,7 @@ const Links = () => {
         </div>
       </div>
 
-      <pre>{JSON.stringify(errors.name?.message)}</pre>
-      <form onSubmit={handleSubmit(submit)} className="container w-full mx-auto shadow-md md:w-3/4 mt-8">
+      <form onSubmit={handleSubmit(submitLink)} className="container w-full mx-auto shadow-md md:w-3/4 mt-8">
         <div className="p-4 bg-gray-100 border-t-2 border-indigo-400 rounded-lg bg-opacity-5">
           <div className="max-w-sm mx-auto md:w-full md:mx-0">
             <div className="inline-flex items-center space-x-4">
@@ -174,116 +175,121 @@ const Links = () => {
         </div>
       </form>
 
-      <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
-        <div className="py-8">
-          <div className="flex flex-row mb-1 sm:mb-0 justify-between w-full">
-            <h2 className="text-2xl leading-tight">
-              Links
-            </h2>
-            <div className="text-end">
-              <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
-                <div className=" relative ">
-                  <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
+      {data && data.length === 0 ?
+        <AlertComponent showIcon={false}>Nenhum link cadastrado</AlertComponent> :
+        (
+          <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
+            <div className="py-8">
+              <div className="flex flex-row mb-1 sm:mb-0 justify-between w-full">
+                <h2 className="text-2xl leading-tight">
+                  Links
+                </h2>
+                <div className="text-end">
+                  <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
+                    <div className=" relative ">
+                      <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
+                    </div>
+                    <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
+                      Filter
+                    </button>
+                  </form>
                 </div>
-                <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                  Filter
-                </button>
-              </form>
-            </div>
-          </div>
-          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
-            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-              <table className="min-w-full leading-normal">
-                <thead>
-                  <tr>
-                    <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                      User
-                    </th>
-                    <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                      Role
-                    </th>
-                    <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                      Created at
-                    </th>
-                    <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                      status
-                    </th>
-                    <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                    </th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {data && data.map((link: Link) => (
-                    <tr key={link.id}>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <div className="flex items-center">
-                          <div className="ml-3">
+              </div>
+              <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+                <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+                  <table className="min-w-full leading-normal">
+                    <thead>
+                      <tr>
+                        <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                          User
+                        </th>
+                        <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                          Role
+                        </th>
+                        <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                          Created at
+                        </th>
+                        <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                          status
+                        </th>
+                        <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {data && data.map((link: Link) => (
+                        <tr key={link.id}>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <div className="flex items-center">
+                              <div className="ml-3">
+                                <p className="text-gray-900 whitespace-no-wrap">
+                                  {link.name}
+                                </p>
+                              </div>
+                            </div>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                             <p className="text-gray-900 whitespace-no-wrap">
-                              {link.name}
+                              {link.publicName}
                             </p>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-900 whitespace-no-wrap">
-                          {link.publicName}
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <p className="text-gray-900 whitespace-no-wrap">
-                          12/09/2020
-                        </p>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                          <span aria-hidden="true" className="absolute inset-0 bg-green-200 opacity-50 rounded-full">
-                          </span>
-                          <span className="relative">
-                            active
-                          </span>
-                        </span>
-                      </td>
-                      <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                          Edit
-                        </a>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-              <div className="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
-                <div className="flex items-center">
-                  <button type="button" className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100">
-                    <svg width={9} fill="currentColor" height={8} className viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
-                      </path>
-                    </svg>
-                  </button>
-                  <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-indigo-500 bg-white hover:bg-gray-100 ">
-                    1
-                  </button>
-                  <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
-                    2
-                  </button>
-                  <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-gray-600 bg-white hover:bg-gray-100">
-                    3
-                  </button>
-                  <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
-                    4
-                  </button>
-                  <button type="button" className="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100">
-                    <svg width={9} fill="currentColor" height={8} className viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                      <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
-                      </path>
-                    </svg>
-                  </button>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              12/09/2020
+                            </p>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                              <span aria-hidden="true" className="absolute inset-0 bg-green-200 opacity-50 rounded-full">
+                              </span>
+                              <span className="relative">
+                                active
+                              </span>
+                            </span>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                              Edit
+                            </a>
+                            <button onClick={() => deleteLink(link.id)}>Delete</button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                  <div className="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
+                    <div className="flex items-center">
+                      <button type="button" className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100">
+                        <svg width={9} fill="currentColor" height={8} className viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
+                          </path>
+                        </svg>
+                      </button>
+                      <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-indigo-500 bg-white hover:bg-gray-100 ">
+                        1
+                      </button>
+                      <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
+                        2
+                      </button>
+                      <button type="button" className="w-full px-4 py-2 border-t border-b text-base text-gray-600 bg-white hover:bg-gray-100">
+                        3
+                      </button>
+                      <button type="button" className="w-full px-4 py-2 border text-base text-gray-600 bg-white hover:bg-gray-100">
+                        4
+                      </button>
+                      <button type="button" className="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100">
+                        <svg width={9} fill="currentColor" height={8} className viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
+                          </path>
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        )}
     </>
   )
 }
