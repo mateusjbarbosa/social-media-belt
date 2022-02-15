f33cc9061b744aa891ee3b2bbd2d5f9993e687c0
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-15 11:31:45 -0300

    feat: prepare links page to get next and previous page

--- pages/app/[tenantId]/links.tsx
+++ pages/app/[tenantId]/links.tsx
@@ -38,7 +38,8 @@ const Links = () => {
   const tenantId = router?.query?.tenantId
   const cursor = router?.query?.cursor ? `?cursor=${router?.query?.cursor}` : ''
 
-  const { data, mutate } = useGet<PaginationWrapper<Link>>(tenantId && `/api/${router?.query?.tenantId}/links${cursor}`)
+  const { data, mutate } =
+    useGet<PaginationWrapper<Link[]>>(tenantId && `/api/${router?.query?.tenantId}/links${cursor}`)
 
   const { handleSubmit, register, formState: { errors } } = useForm<NewLinkForm>({
     resolver: yupResolver(newLinkSchema)
@@ -264,8 +265,12 @@ const Links = () => {
                   </table>
                   <div className="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
                     <div className="flex items-center">
-                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.items[data?.items?.length - 1].id}`}>
-                        <button type="button" className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100">
+                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.cursor}&take=${Math.abs(Number(data?.take)) * -1}`}>
+                        <button
+                          type="button"
+                          className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100"
+                          disabled={!data?.hasPrevious}
+                        >
                           <svg width={9} fill="currentColor" height={8} viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
                             </path>
@@ -273,8 +278,12 @@ const Links = () => {
                         </button>
                       </NextLink>
 
-                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.items[data?.items?.length - 1].id}`}>
-                        <button type="button" className="w-full p-4 border-t border-b border-r text-base  rounded-r-xl text-gray-600 bg-white hover:bg-gray-100">
+                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.cursor}`}>
+                        <button
+                          type="button"
+                          className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100"
+                          disabled={!data?.hasNext}
+                        >
                           <svg width={9} fill="currentColor" height={8} viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
                             </path>
