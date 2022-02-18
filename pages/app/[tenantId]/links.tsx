cda821cfdf58a228ff130e4997d967cfd84719ba
Mateus José Barbosa <46843918+mateusjbarbosa@users.noreply.github.com>
2022-02-18 11:59:33 -0300

    feat: update links page and update to new logic to paginate links and add other infos

--- pages/app/[tenantId]/links.tsx
+++ pages/app/[tenantId]/links.tsx
@@ -8,14 +8,15 @@ import * as yup from "yup"
 
 import { Link } from '@prisma/client'
 
-import { useGet } from "hooks/api"
-import { fetchPost, fetchDelete } from "lib/fetch"
-
-import { PaginationWrapper } from '../../api/[tenantId]/links'
-
 import AlertComponent from "components/Alert"
 import Subtitle from "components/Headings/Subtitle"
 import Title from "components/Headings/Title"
+
+import { useGet } from "hooks/api"
+
+import { fetchPost, fetchDelete } from "lib/fetch"
+
+import { LinkPaginationWrapper } from "services/links"
 
 type NewLinkForm = {
   name: string
@@ -38,14 +39,14 @@ const Links = () => {
   const tenantId = router?.query?.tenantId
   const cursor = router?.query?.cursor ? `?cursor=${router?.query?.cursor}` : ''
 
-  const { data, mutate } =
-    useGet<PaginationWrapper<Link[]>>(tenantId && `/api/${router?.query?.tenantId}/links${cursor}`)
-
   const { handleSubmit, register, formState: { errors } } = useForm<NewLinkForm>({
     resolver: yupResolver(newLinkSchema)
   })
 
-  const submitLink: SubmitHandler<NewLinkForm> = async (inputs: NewLinkForm) => {
+  const { data, mutate } =
+    useGet<LinkPaginationWrapper>(tenantId && `/api/${router?.query?.tenantId}/links${cursor}`)
+
+  const submit: SubmitHandler<NewLinkForm> = async (inputs: NewLinkForm) => {
     await fetchPost({ url: `api/${tenantId}/links`, data: inputs })
 
     await mutate()
@@ -82,7 +83,7 @@ const Links = () => {
         </div>
       </div>
 
-      <form onSubmit={handleSubmit(submitLink)} className="container w-full mx-auto shadow-md md:w-3/4 mt-8">
+      <form onSubmit={handleSubmit(submit)} className="container w-full mx-auto md:w-3/4 mt-8">
         <div className="p-4 bg-gray-100 border-t-2 border-indigo-400 rounded-lg bg-opacity-5">
           <div className="max-w-sm mx-auto md:w-full md:mx-0">
             <div className="inline-flex items-center space-x-4">
@@ -184,7 +185,7 @@ const Links = () => {
       {data && data?.items?.length === 0 ?
         <AlertComponent showIcon={false}>Nenhum link cadastrado</AlertComponent> :
         (
-          <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
+          <div className="container w-full mx-auto md:w-3/4 mt-8">
             <div className="py-8">
               <div className="flex flex-row mb-1 sm:mb-0 justify-between w-full">
                 <h2 className="text-2xl leading-tight">
@@ -193,10 +194,10 @@ const Links = () => {
                 <div className="text-end">
                   <form className="flex flex-col md:flex-row w-3/4 md:w-full max-w-sm md:space-x-3 space-y-3 md:space-y-0 justify-center">
                     <div className=" relative ">
-                      <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="name" />
+                      <input type="text" id="&quot;form-subscribe-Filter" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Digite o termo que está buscando" />
                     </div>
                     <button className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
-                      Filter
+                      Filtrar
                     </button>
                   </form>
                 </div>
@@ -207,18 +208,22 @@ const Links = () => {
                     <thead>
                       <tr>
                         <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                          User
+                          Nome
                         </th>
                         <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                          Role
+                          Identificador
                         </th>
                         <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                          Created at
+                          Destino
                         </th>
                         <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
-                          status
+                          Criado em
                         </th>
                         <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                          Situação
+                        </th>
+                        <th scope="col" className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal">
+                          Ações
                         </th>
                       </tr>
                     </thead>
@@ -230,18 +235,25 @@ const Links = () => {
                               <div className="ml-3">
                                 <p className="text-gray-900 whitespace-no-wrap">
                                   {link.name}
+                                  <br />
+                                  <span className="text-xs text-gray-500">{link.publicName}</span>
                                 </p>
                               </div>
                             </div>
                           </td>
                           <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                             <p className="text-gray-900 whitespace-no-wrap">
-                              {link.publicName}
+                              {link.slug}
                             </p>
                           </td>
                           <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                             <p className="text-gray-900 whitespace-no-wrap">
-                              12/09/2020
+                              {link.destination}
+                            </p>
+                          </td>
+                          <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {new Date(link.createdAt).toLocaleDateString()}
                             </p>
                           </td>
                           <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
@@ -249,15 +261,15 @@ const Links = () => {
                               <span aria-hidden="true" className="absolute inset-0 bg-green-200 opacity-50 rounded-full">
                               </span>
                               <span className="relative">
-                                active
+                                ativo
                               </span>
                             </span>
                           </td>
                           <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                             <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                              Edit
+                              Editar
                             </a>
-                            <button onClick={() => deleteLink(link.id)}>Delete</button>
+                            <button onClick={() => deleteLink(link.id)}>Remover</button>
                           </td>
                         </tr>
                       ))}
@@ -265,11 +277,11 @@ const Links = () => {
                   </table>
                   <div className="px-5 bg-white py-5 flex flex-col xs:flex-row items-center xs:justify-between">
                     <div className="flex items-center">
-                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.cursor}&take=${Math.abs(Number(data?.take)) * -1}`}>
+                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.prevCursor}`}>
                         <button
                           type="button"
                           className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100"
-                          disabled={!data?.hasPrevious}
+                          disabled={!data?.prevCursor}
                         >
                           <svg width={9} fill="currentColor" height={8} viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1427 301l-531 531 531 531q19 19 19 45t-19 45l-166 166q-19 19-45 19t-45-19l-742-742q-19-19-19-45t19-45l742-742q19-19 45-19t45 19l166 166q19 19 19 45t-19 45z">
@@ -278,11 +290,11 @@ const Links = () => {
                         </button>
                       </NextLink>
 
-                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.cursor}`}>
+                      <NextLink href={`/app/${tenantId}/links?cursor=${data?.nextCursor}`}>
                         <button
                           type="button"
-                          className="w-full p-4 border text-base rounded-l-xl text-gray-600 bg-white hover:bg-gray-100"
-                          disabled={!data?.hasNext}
+                          className="w-full p-4 border text-base rounded-r-xl text-gray-600 bg-white hover:bg-gray-100"
+                          disabled={!data?.nextCursor}
                         >
                           <svg width={9} fill="currentColor" height={8} viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
                             <path d="M1363 877l-742 742q-19 19-45 19t-45-19l-166-166q-19-19-19-45t19-45l531-531-531-531q-19-19-19-45t19-45l166-166q19-19 45-19t45 19l742 742q19 19 19 45t-19 45z">
